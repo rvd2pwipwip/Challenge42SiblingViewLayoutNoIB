e98c8779a1053d36c6b41840154f69f96507fff2
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-27 16:32:59 -0500

    finished challenge 42 programmatically

--- Challenge42SiblingViewLayoutNoIB/ViewController.swift
+++ Challenge42SiblingViewLayoutNoIB/ViewController.swift
@@ -12,9 +12,61 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupRedView()
+        setupGreenView()
+        setupBlueView()
+    }
+    
+    private func setupRedView() {
+        view.addSubview(redView)
+        NSLayoutConstraint.activate([
+            redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            redView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            redView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+        ])
+    }
+    
+    private func setupGreenView() {
+        view.addSubview(greenView)
+        NSLayoutConstraint.activate([
+            greenView.topAnchor.constraint(equalTo: redView.bottomAnchor, constant: 25),
+            greenView.leadingAnchor.constraint(equalTo: redView.leadingAnchor),
+            greenView.trailingAnchor.constraint(equalTo: redView.trailingAnchor),
+            greenView.heightAnchor.constraint(equalTo: redView.heightAnchor)
+        ])
+    }
+    
+    private func setupBlueView() {
+        view.addSubview(blueView)
+        NSLayoutConstraint.activate([
+            blueView.topAnchor.constraint(equalTo: greenView.bottomAnchor, constant: 25),
+            blueView.leadingAnchor.constraint(equalTo: redView.leadingAnchor),
+            blueView.trailingAnchor.constraint(equalTo: redView.trailingAnchor),
+            blueView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            blueView.heightAnchor.constraint(equalTo: redView.heightAnchor)
+        ])
     }
 
+    private let redView: UIView = {
+        let rv = UIView()
+        rv.backgroundColor = .red
+        rv.translatesAutoresizingMaskIntoConstraints = false
+        return rv
+    }()
+    
+    private let greenView: UIView = {
+        let gv = UIView()
+        gv.backgroundColor = .green
+        gv.translatesAutoresizingMaskIntoConstraints = false
+        return gv
+    }()
+    
+    private let blueView: UIView = {
+        let bv = UIView()
+        bv.backgroundColor = .blue
+        bv.translatesAutoresizingMaskIntoConstraints = false
+        return bv
+    }()
 
 }
 
